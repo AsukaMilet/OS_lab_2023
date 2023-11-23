8144c6c8d530c930af85a61e54d9974071b93a1a
AsukaMilet <zetaxcz@outlook.com>
2023-11-23 19:09:09 +0800

    Implement LRU-K replace policy

--- include/memory/replacer.h
+++ include/memory/replacer.h
@@ -4,9 +4,9 @@
 #define i_type UIList
 #define i_key size_t
 // Include type size_t
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdbool.h>
 #include "stc/clist.h"
 
 typedef int32_t frame_id_t;
@@ -21,25 +21,72 @@ typedef struct LRUKNode {
 //===----------------------------------------------------------------------===//
 // LRUKNode statement
 //===----------------------------------------------------------------------===//
-void LRUKNodeInit(LRUKNode *node, size_t k, frame_id_t fid);
+LRUKNode LRUKNodeInit(size_t k, frame_id_t fid);
 
+LRUKNode LRUKNode_clone(LRUKNode node);
+
+// Destructor for LRUKNode
 void LRUKNodeDestroy(LRUKNode *node);
 
+// Check whether a frame is evictable
 bool IsEvictable(LRUKNode *node);
 
+// Set whether a frame is evictable
 void SetEvictable(LRUKNode *node, bool set_evictable);
 
+// Record the access for the given frame
 void FrameAccessed(LRUKNode *node, size_t timestamp);
 
+// Get the number of access for the given frame
 size_t TimestampNum(LRUKNode *node);
 
+// Get the earliest access for the given frame
 size_t OldestTimestamp(LRUKNode *node);
 
 frame_id_t GetFrameId(LRUKNode *node);
 
+// Get the kth access for the given frame
 size_t KthTimestamp(LRUKNode *node);
 //===----------------------------------------------------------------------===//
 // Replacer statement
 //===----------------------------------------------------------------------===//
+#define i_type HashTable
+#define i_key frame_id_t
+#define i_valclass LRUKNode
+#define i_valclone LRUKNode_clone
+#define i_valdrop LRUKNodeDestroy
+#include "stc/cmap.h"
 
+typedef struct LRUKReplacer {
+  HashTable node_store_;
+  size_t current_timestamp_;
+  size_t curr_size_;      // The number of evictable frames
+  size_t replacer_size_;  // Maximum number of frames in the replacer
+  size_t k_;
+} Replacer;
+
+// Initialize the replacer
+Replacer *ReplacerInit(size_t num_frames, size_t k);
+
+// Destroy the replacer to avoid memory leak
+void ReplacerDestroy(Replacer *replacer);
+
+/**
+ * @brief Find the frame with largest backward k-distance and evict that frame.
+ *
+ * @param replacer
+ * @param[out] fid id of frame that is evicted.
+ * @return true
+ * @return false
+ */
+bool ReplacerEvict(Replacer *replacer, frame_id_t *fid);
+
+// Record the access of a frame
+void ReplacerRecordAccess(Replacer *replacer, frame_id_t fid);
+
+// Toggle whether a frame is evictable or non-evictable
+void ReplacerSetEvictable(Replacer *replacer, frame_id_t fid, bool set_evictable);
+
+// Return replacer's size, which tracks the number of evictable frames
+size_t ReplacerSize(Replacer *replacer);
 #endif
--- src/memory/replacer.c
+++ src/memory/replacer.c
@@ -1,26 +1,27 @@
 #include "memory/replacer.h"
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
 
 //===----------------------------------------------------------------------===//
 // LRUKNode Implementation
 //===----------------------------------------------------------------------===//
-void LRUKNodeInit(LRUKNode *node, size_t k, frame_id_t fid) {
-  node->history_ = UIList_init();
-  node->k_ = k;
-  node->fid_ = fid;
-  node->is_evictable_ = false;
+LRUKNode LRUKNodeInit(size_t k, frame_id_t fid) {
+  LRUKNode node;
+  node.history_ = UIList_init();
+  node.k_ = k;
+  node.fid_ = fid;
+  node.is_evictable_ = false;
+  return node;
 }
 
-void LRUKNodeDestroy(LRUKNode *node) {
-  UIList_drop(&node->history_);
-}
+void LRUKNodeDestroy(LRUKNode *node) { UIList_drop(&node->history_); }
 
-bool IsEvictable(LRUKNode *node) {
-  return node->is_evictable_;
-}
+// Check whether a frame is evictable
+bool IsEvictable(LRUKNode *node) { return node->is_evictable_; }
 
-void SetEvictable(LRUKNode *node, bool set_evictable) {
-  node->is_evictable_ = set_evictable;
-}
+// Set whether a frame is evictable
+void SetEvictable(LRUKNode *node, bool set_evictable) { node->is_evictable_ = set_evictable; }
 
 void FrameAccessed(LRUKNode *node, size_t timestamp) {
   // New timestamp is added to the front of the timestamp list
@@ -37,9 +38,7 @@ size_t OldestTimestamp(LRUKNode *node) {
   return *UIList_back(&node->history_);
 }
 
-frame_id_t GetFrameId(LRUKNode *node) {
-  return node->fid_;
-}
+frame_id_t GetFrameId(LRUKNode *node) { return node->fid_; }
 
 // Get the kth timestamp
 size_t KthTimestamp(LRUKNode *node) {
@@ -47,6 +46,113 @@ size_t KthTimestamp(LRUKNode *node) {
   iter = UIList_advance(iter, node->k_ - 1);
   return *iter.ref;
 }
+
+LRUKNode LRUKNode_clone(LRUKNode node) {
+  node.history_ = UIList_clone(node.history_);
+  node.fid_ = node.fid_;
+  node.k_ = node.k_;
+  node.is_evictable_ = node.is_evictable_;
+  return node;
+}
 //===----------------------------------------------------------------------===//
 // Replacer Implementation
 //===----------------------------------------------------------------------===//
+Replacer *ReplacerInit(size_t num_frames, size_t k) {
+  Replacer *replacer = (Replacer *)malloc(sizeof(Replacer));
+  replacer->node_store_ = HashTable_init();
+  replacer->current_timestamp_ = 0;
+  replacer->curr_size_ = 0;
+  replacer->replacer_size_ = num_frames;
+  replacer->k_ = k;
+  return replacer;
+}
+
+void ReplacerDestroy(Replacer *replacer) {
+  HashTable_drop(&replacer->node_store_);
+  free(replacer);
+}
+
+bool ReplacerEvict(Replacer *replacer, frame_id_t *frame_id) {
+  // If no frame is evictable, return false directly
+  if (replacer->curr_size_ == 0) {
+    return false;
+  }
+
+  frame_id_t evict_frame_id;
+  size_t k_distance = 0;
+  size_t oldest_recent_timestamp = UINT_MAX;
+
+  c_forpair(frame_id, frame, HashTable, replacer->node_store_) {
+    // Check whether the frame is evictable
+    if (IsEvictable(_.frame)) {
+      // If frame access is less than k-distance, k-distance is +inf
+      if (TimestampNum(_.frame) < replacer->k_) {
+        k_distance = UINT_MAX;
+        if (OldestTimestamp(_.frame) < oldest_recent_timestamp) {
+          // If the k-distance are equal, choose the earliest frame had been accessed
+          oldest_recent_timestamp = OldestTimestamp(_.frame);
+          evict_frame_id = *_.frame_id;
+        }
+      } else {
+        // Calculate the k-distance, find the maximum
+        if (replacer->current_timestamp_ - KthTimestamp(_.frame) > k_distance) {
+          k_distance = replacer->current_timestamp_ - KthTimestamp(_.frame);
+          evict_frame_id = *_.frame_id;
+        } else if (replacer->current_timestamp_ - KthTimestamp(_.frame) == k_distance) {
+          // If the k-distance are equal, choose the earliest frame had been accessed
+          if (OldestTimestamp(_.frame) < oldest_recent_timestamp) {
+            oldest_recent_timestamp = OldestTimestamp(_.frame);
+            evict_frame_id = *_.frame_id;
+          }
+        }
+      }
+    }
+  }
+
+  *frame_id = evict_frame_id;
+  HashTable_erase(&replacer->node_store_, evict_frame_id);
+  replacer->curr_size_--;
+  return true;
+}
+
+void ReplacerRecordAccess(Replacer *replacer, frame_id_t frame_id) {
+  // Frame id is invalid
+  if ((size_t)frame_id > replacer->replacer_size_) {
+    fprintf(stderr, "frame_id should be less than replacer_size\n");
+  }
+
+  // If the frame has not been seen before, insert it
+  LRUKNode temp = LRUKNodeInit(replacer->k_, frame_id);
+  HashTable_insert(&replacer->node_store_, frame_id, temp);
+  LRUKNode *frame_ptr = HashTable_at_mut(&replacer->node_store_, frame_id);
+  FrameAccessed(frame_ptr, replacer->current_timestamp_);
+  replacer->current_timestamp_++;
+}
+
+void ReplacerSetEvictable(Replacer *replacer, frame_id_t frame_id, bool set_evictable) {
+  // If the frame doesn't exist, directly return
+  if (!HashTable_contains(&replacer->node_store_, frame_id)) {
+    fprintf(stderr, "Frame %d doesn't exist\n", frame_id);
+    return;
+  }
+
+  LRUKNode *frame_ptr = HashTable_at_mut(&replacer->node_store_, frame_id);
+
+  bool original_evictable = IsEvictable(frame_ptr);
+  // If the evictable field of the given frame has not changed, return directly
+  if (set_evictable == original_evictable) {
+    return;
+  }
+
+  // A frame was previously non-evictable and is to be set to evictable
+  if (!original_evictable) {
+    SetEvictable(frame_ptr, set_evictable);
+    replacer->curr_size_++;
+  } else {
+    // A frame was previously evictable and is to be set to non-evictable
+    SetEvictable(frame_ptr, set_evictable);
+    replacer->curr_size_--;
+  }
+}
+
+size_t ReplacerSize(Replacer *replacer) { return replacer->curr_size_; }
